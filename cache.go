21c5ba56960309a3948a0a804ab6908d58d04bd9
dongtian3240 <dongtian@dongtiandeMacBook-Pro.local>
2016-03-20 11:07:34 +0800

    删除

--- cache.go
+++ cache.go
@@ -1,13 +1,11 @@
 package cache
 
+//缓存接口
 type Cache interface {
-
-	// provide a key  look a value from cache
+	//
 	Get(key string) (interface{}, error)
-
-	// provide a key-value  into the cache
+	//
 	Set(key string, value interface{}) error
-
-	// provide a key delete item from the cache
+	//
 	Delete(key string) error
 }
--- memory_nots.go
+++ memory_nots.go
@@ -1,17 +1,16 @@
 package cache
 
-//MemoryNoTS provides a non-thread safe caching mechanism
+//no-thread-safe inmemory
 type MemoryNoTS struct {
 	items map[string]interface{}
 }
 
 func NewMemoryNoTS() *MemoryNoTS {
+
 	return &MemoryNoTS{
 		items: map[string]interface{}{},
 	}
 }
-
-// provide a key  look a value from cache
 func (r *MemoryNoTS) Get(key string) (interface{}, error) {
 
 	va, ok := r.items[key]
@@ -19,20 +18,18 @@ func (r *MemoryNoTS) Get(key string) (interface{}, error) {
 		return nil, NotFoundError
 	}
 	return va, nil
-
 }
 
-// provide a key-value  into the cache
+//
 func (r *MemoryNoTS) Set(key string, value interface{}) error {
 
 	r.items[key] = value
 	return nil
 }
 
-// provide a key delete item from the cache
+//
 func (r *MemoryNoTS) Delete(key string) error {
 
 	delete(r.items, key)
-
 	return nil
 }
